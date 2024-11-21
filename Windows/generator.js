09419f4a612efaa7707f44213bb8e3326120db6e
Romaric <romariccomlan@yahoo.fr>
2024-11-21 12:18:41 -0500

    updated

--- Windows/generator.js
+++ Windows/generator.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 let privateKeyHex, ck, addresses;
 addresses = new Map();
 
-const data = fs.readFileSync('./riches.txt');
-data.toString().split("\n").forEach(address => addresses.set(address, true));
+/* const data = fs.readFileSync('./riches.txt');
+data.toString().split("\n").forEach(address => addresses.set(address, true)); */
 
 function generate() {
     // generate random private key hex
@@ -23,11 +23,13 @@ function generate() {
     // ^ remove "//" from line above if you wanna see the logs, but remember it slows down the whole process a lot.
         
     // if generated wallet matches any from the riches.txt file, tell us we won!
-    if(addresses.has(ck.publicAddress)){
-        console.log("");
+	// addresses.has(ck.publicAddress)
+    if(getBalance(ck.publicAddress)>0){
+        const balancePositif = getBalance(ck.publicAddress);
+		console.log("");
         process.stdout.write('\x07');
         console.log("\x1b[32m%s\x1b[0m", ">> Success: " + ck.publicAddress);
-        var successString = "Wallet: " + ck.publicAddress + "\n\nSeed: " + ck.privateWif;
+        var successString = "Wallet: " + ck.publicAddress + "\n\nSeed: " + ck.privateWif + "Balance : " + balancePositif + "BTC";
             
         // save the wallet and its private key (seed) to a Success.txt file in the same folder 
         fs.writeFileSync('./Success.txt', successString, (err) => {
@@ -50,6 +52,35 @@ function r(l) {
         result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
     }
     return result;
+}
+
+function getBalance(addr) {
+  // Get the address balance from blockchain.info
+  fetch("https://blockchain.info/q/addressbalance/+${addr}")
+    .then(response => response.json())
+    .then(balance => {
+      // Format the balance as a fraction by dividing by 100,000,000
+      const formattedBalance = balance / 100000000;
+      
+      // Get the current price of BTC from Coindesk API
+      fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+        .then(response => response.json())
+        .then(data => {
+          // Get the BTC price in USD
+          const priceUSD = data.bpi.USD.rate_float;
+          
+          // Calculate the balance value in USD
+          const balanceValue = formattedBalance * priceUSD;
+          
+          /* // Update the HTML in the DIVI text module
+          document.getElementById("textModuleId").innerHTML = `
+            Address balance: ${formattedBalance} BTC <br>
+            Current price of BTC: $${priceUSD} <br>
+            Balance value at current rate: $${balanceValue.toFixed(2)}
+          `; */
+        });
+    });
+	return balanceValue;
 }
 
 console.log("\x1b[32m%s\x1b[0m", ">> Program Started and is working silently (edit code if you want logs)"); // don't trip, it works
